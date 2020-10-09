c687d1111c8e35e43dd8606e35c49985dea878e7
Kevin Espinoza <oportokevin15@gmail.com>
2020-10-09 12:49:57 -0300

    Locallibreria: creando proyecto

--- locallibreria/catalogo/models.py
+++ locallibreria/catalogo/models.py
@@ -1,3 +1,4 @@
 from django.db import models
-
+from django.urls import reverse  		#redirecciona una url de un libro al browser 
+import uuid  							#se utiliza para definir atributos clave (pk)
 # Create your models here.
